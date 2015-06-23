93f507a496d5a082b4717d19fbf6252ec3623ee1
SergeyVolodko <volodkosm@gmail.com>
2015-06-23 17:43:06 +0300

    Ex 1 Create a simple String calculator with a method int Add(string numbers)

--- KataCalculator/KataCalculator.Tests/CalculatorTests.cs
+++ KataCalculator/KataCalculator.Tests/CalculatorTests.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using StringCalculator;
+using Xunit;
 using Xunit.Extensions;
 
 namespace KataCalculator.Tests
@@ -7,8 +7,32 @@ namespace KataCalculator.Tests
     public class CalculatorTests
     {
         [Theory, CalculatorTestConventions]
-        public void TestMethod1()
+        public void AddEmptyReturnsCorrectResult(Calculator sut)
         {
+            var numbers = "";
+            var actual = sut.Add(numbers);
+            Assert.Equal(0, actual);
+        }
+
+        [Theory, CalculatorTestConventions]
+        public void AddSingleNumberReturnsCorrectResult(
+            Calculator sut, 
+            int expected)
+        {
+            var numbers = expected.ToString();
+            var actual = sut.Add(numbers);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory, CalculatorTestConventions]
+        public void AddTwoNumbersReturnsCorrectResult(
+            Calculator sut, 
+            int x, 
+            int y)
+        {
+            var numbers = string.Join(",", x, y);
+            var actual = sut.Add(numbers);
+            Assert.Equal(x + y, actual);
         }
     }
 }
---
+++ KataCalculator/StringCalculator/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace StringCalculator
+{
+    public class Calculator
+    {
+        public int Add(string numbers)
+        {
+            return numbers
+                .Split(",".ToArray(), StringSplitOptions.RemoveEmptyEntries)
+                .Select(s=>int.Parse(s))
+                .Sum();
+        }
+    }
+}
